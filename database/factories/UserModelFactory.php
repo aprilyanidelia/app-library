d29d0b872f88bc65adba73dc2e4c9c92c9b8aa46
Delia aprilyani <aprilyanidelia127@gmail.com>
2024-08-10 11:39:59 +0700

    add factory user

--- database/factories/UserModelFactory.php
+++ database/factories/UserModelFactory.php
@@ -2,25 +2,21 @@
 
 namespace Database\Factories;
 
+use App\Model\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
  */
-class UserFactory extends Factory
+class UserModelFactory extends Factory
 {
-    /**
-     * The current password being used by the factory.
-     */
-    protected static ?string $password;
-
     /**
      * Define the model's default state.
      *
      * @return array<string, mixed>
      */
+    protected $model= User::class;
+
     public function definition(): array
     {
         return [
@@ -30,15 +26,5 @@ class UserFactory extends Factory
             'nm_lengkap'=>$this->faker->name(),
             'alamat'=>$this->faker->address(),
         ];
-    }
-
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
-    public function unverified(): static
-    {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
     }
 }
